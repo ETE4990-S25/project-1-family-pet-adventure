4f1c383805c0bb3f93a302625c74b5b1fa6a7610
Joanna Escobar <kn6rpb@gmail.com>
2025-02-27 08:52:10 -0800

    rempved effectiveness parameter

--- Exploring.py
+++ Exploring.py
@@ -2,32 +2,32 @@
 #Game Classes as of 2/7/2025
 class Exploring(object):
     """Moving around the space."""
-    def __init__(self, jump, sneak, effectiveness=0):
+    def __init__(self, jump, sneak):
         """ Initialize jump, sneak, and effectiveness."""
         self.jump = jump
         self.sneak = sneak
-        self.effectiveness = effectiveness
-    def how_effective(self, target):
-        target = target - self.effectiveness
+        
+    
 
 class Cat_Moves(Exploring):
     """initializes basic actions a cat can do"""
-    def __init__(self, jump, sneak, effectiveness=0):
+    def __init__(self, jump, sneak):
         """sets up basic actions"""
-        super().__init__(self, jump, sneak, effectiveness)
+        super().__init__(self, jump, sneak)
         self.poision = True
-    def furniture_leap(self, effectiveness=0):
-        super().furniture_leap(self, effectiveness)
-    def climb(self, effectiveness=0):
-        super().climb(self, effectiveness)
+    def furniture_leap(self):
+        super().furniture_leap(self)
+    def climb(self):
+        super().climb(self)
 
 class Dog_Moves(Exploring):
     """initializes basic actions a dog can do"""
-    def __init__(self, jump, sneak, effectiveness=0):
+    def __init__(self, jump, sneak):
         """sets up basic actions"""
-        super().__init__(self, jump, sneak, effectiveness)
+        super().__init__(self, jump, sneak)
         self.poision = True
-    def sprint(self, effectiveness=0):
-        super().sprint(self, effectiveness)
-    def paw_stand(self, effectiveness=0):
-        super().paw_stand(self, effectiveness)
+
+    def sprint(self, ):
+        super().sprint(self)
+    def paw_stand(self):
+        super().paw_stand(self)
