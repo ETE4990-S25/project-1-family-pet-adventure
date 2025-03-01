1c43b91373c8bef1786f8d818520979866f71dfb
Sukhdeep29 <sukhdeepkaur@cpp.edu>
2025-02-28 20:46:44 -0800

    Created a new file

---
+++ Players.py
@@ -0,0 +1,5 @@
+class FamilyPetAdventure(object):
+    """Creates a class named Family Pet Adventure."""
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
