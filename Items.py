5d7fd79c71318f3c3eba37a0d8e43fd0c0f49228
Joanna Escobar <kn6rpb@gmail.com>
2025-02-28 19:56:29 -0800

    housekeeping in Items.py

--- Items.py
+++ Items.py
@@ -1,21 +1,25 @@
 #Items.py
 #General space for the items that may be encountered in the game
 
-class Items(object):
+class Things(object):
     """Initializes objects the player can interact with."""
     def __init__(self, name, uses):
         """Sets up the basic details of the object."""
 
         self.name = name
-        self.uses = uses
+        self.uses = uses #determines how many uses an object gets
 
-        uses = 5
 
     def use_object(self):
+        """Iterates the number of uses an object has."""
         uses = uses -1
-        print("You have used this object! Only "+ uses + " left!")
+        
+        if uses != 0:
+            print("You have used this object! Only "+ uses + " left!")
+        elif uses == 0:
+            print("You have used this object! No more uses left.")
 
-class Key(Items):
+class Key(Things):
     """Initializes keys that the player is trying to find."""
     def __Init__(self, ):
         super().__init__
